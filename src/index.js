334ac045b65f9fb21ae94e50b460ebdcb6ee96da
Atit Mistry <atitmistry89@gmail.com>
2022-10-21 19:27:23 +0530

    v14

--- src/index.js
+++ src/index.js
@@ -1,23 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js'
 import Home from "./pages/Home";
 import Filter from "./pages/Filter";
-import Layout from "./pages/Layout";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="filter" element={<Filter />} />
-          {/* <Route path="*" element={<Home />} /> */}
-        </Route>
+        <Route exact path='/' element={< Home />}></Route>
+        <Route exact path='/filter' element={< Filter />}></Route>
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
